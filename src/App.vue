683d2a063bc3575837ec0cabe98b0b0565d880ab
vrusi <me@vrusinkova.com>
2022-09-01 18:41:12 +0200

    Put navbar in a separate component

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,16 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
+import NavBar from "./components/NavBar.vue";
 </script>
 
 <template>
-  <link href="https://fonts.googleapis.com/css?family=Baloo+2:400,800&display=swap" rel="stylesheet">
-  <header>
-    <nav>
-      <RouterLink to="/">App</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
-  </header>
+  <link
+    href="https://fonts.googleapis.com/css?family=Baloo+2:400,800&display=swap"
+    rel="stylesheet"
+  />
+  <NavBar />
   <RouterView />
 </template>
 
 <style scoped>
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">App</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<style>
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+</style>
