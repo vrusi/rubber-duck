7b4f37729a7fe3b2549da11ec385834e88fbcbdf
vrusi <me@vrusinkova.com>
2022-09-01 18:45:16 +0200

    Put the typing prompt into a separate component

---
+++ src/components/Prompt.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div id="wrapper">
+    <div id="paper">
+      <textarea
+        placeholder="I'm implementing user registration and..."
+        id="text"
+        name="text"
+        rows="4"
+        cols="50"
+        style="word-wrap: break-word; resize: none"
+      ></textarea>
+    </div>
+  </div>
+
+  <button class="button-17" role="button">What am I doing wrong?</button>
+</template>
+
+<style></style>
--- src/views/RubberDuckView.vue
+++ src/views/RubberDuckView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import SpeechBubble from "../components/SpeechBubble.vue";
 import Duck from "../components/Duck.vue";
+import Prompt from "../components/Prompt.vue";
 </script>
 
 <template>
@@ -9,21 +10,7 @@ import Duck from "../components/Duck.vue";
       <h1>Let's debug!</h1>
       <SpeechBubble msg="What's your problem?" />
       <Duck />
-
-      <div id="wrapper">
-        <div id="paper">
-          <textarea
-            placeholder="I'm implementing user registration and..."
-            id="text"
-            name="text"
-            rows="4"
-            cols="50"
-            style="word-wrap: break-word; resize: none"
-          ></textarea>
-        </div>
-      </div>
-
-      <button class="button-17" role="button">What am I doing wrong?</button>
+      <Prompt />
     </div>
   </main>
 </template>
