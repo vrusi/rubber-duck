d73ef25f9303320c459063ca29bffee42ecfc127
vrusi <me@vrusinkova.com>
2022-09-02 23:52:54 +0200

    Add about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,40 @@
 <template>
   <div class="container">
-    <h1>This is an about page</h1>
+    <h1>About Rubber Duck</h1>
+    <p>
+      This app is here to help you find and solve your problems. Based on the
+      <a href="https://en.wikipedia.org/wiki/Rubber_duck_debugging"
+        >Rubber duck debuging method</a
+      >, it is mostly focused on software engineers. However, everyone is
+      welcome to use the duck.
+    </p>
+    <br />
+    <p>
+      Simply start by explaining your problem to the duck as specifically as
+      possible - remember that the duck does not know anything about your
+      problem before you start talking to it. The idea is that by being forced
+      to put your ideas into words, you are forced to confront the parts of the
+      problems you thought you understood but did not. Usually, this is enough
+      to figure where the issue was and how to fix it.
+    </p>
+    <br />
+    <p>
+      Check out the rubber duck debugging method on
+      <a href="https://en.wikipedia.org/wiki/Rubber_duck_debugging">wikipedia</a
+      >.
+    </p>
+    <h1>About me</h1>
+    <p>Hey, I'm Veronika.</p>
+    <br />
+    <p>
+      I'm a full-stack developer from Slovakia. Feel free to check out my
+      <a href="https://github.com/vrusi">GitHub</a> or
+      <a href="https://www.linkedin.com/in/veronika-rusinkova-6b012718a/"
+        >LinkedIn</a
+      >. You can also contact me at
+      <a href="mailto: me@vrusinkova.com">me@vrusinkova.com</a>.
+    </p>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<style></style>
