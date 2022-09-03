886c8f9187e7db009fd7be66d9a0e5f0609236d8
vrusi <me@vrusinkova.com>
2022-09-03 20:16:13 +0200

    Update text

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,34 +4,33 @@
     <p>
       This app is here to help you find and solve your problems. Based on the
       <a href="https://en.wikipedia.org/wiki/Rubber_duck_debugging"
-        >Rubber duck debuging method</a
-      >, it is mostly focused on software engineers. However, everyone is
-      welcome to use the duck.
+        >rubber duck debuging method</a
+      >, it is mostly focused on helping software engineers to get rid of the
+      bugs in their code. However, everyone is welcome to consult the duck with
+      any sort of problem.
     </p>
     <br />
     <p>
-      Simply start by explaining your problem to the duck as specifically as
-      possible - remember that the duck does not know anything about your
-      problem before you start talking to it. The idea is that by being forced
-      to put your ideas into words, you are forced to confront the parts of the
-      problems you thought you understood but did not. Usually, this is enough
-      to figure where the issue was and how to fix it.
+      Begin by explaining your problem to the duck. Remember to be as specific
+      as possible; the duck does not know anything about your problem, you must
+      introduce it to the topic.
     </p>
     <br />
     <p>
-      Check out the rubber duck debugging method on
-      <a href="https://en.wikipedia.org/wiki/Rubber_duck_debugging">wikipedia</a
-      >.
+      We often think we understand a problem and fail to realise that we have
+      missed possible solutions. By having to explain the problem to someone who
+      knows nothing about it, one is forced to confront all parts and
+      perspectives of it, which is often enough to find the solution.
     </p>
     <h1>About me</h1>
     <p>Hey, I'm Veronika.</p>
-    <br />
     <p>
-      I'm a full-stack developer from Slovakia. Feel free to check out my
+      I'm a full-stack developer based in Bratislava, Slovakia. Feel free to
+      check out my
       <a href="https://github.com/vrusi">GitHub</a> or
       <a href="https://www.linkedin.com/in/veronika-rusinkova-6b012718a/"
         >LinkedIn</a
-      >. You can also contact me at
+      >. You can also reach me at
       <a href="mailto: me@vrusinkova.com">me@vrusinkova.com</a>.
     </p>
   </div>
