b01baa30e8dcea22978b36d2bcb954d82d586ce1
vrusi <me@vrusinkova.com>
2022-09-03 21:15:54 +0200

    Wrap view in <main/>

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,43 +1,49 @@
 <template>
-  <div class="container">
-    <h1>About Rubber Duck</h1>
-    <p>
-      This app is here to help you find and solve your problems. Based on the
-      <a href="https://en.wikipedia.org/wiki/Rubber_duck_debugging"
-        >rubber duck debugging method</a
-      >, it is mostly focused on helping software engineers to get rid of the
-      bugs in their code. However, everyone is welcome to consult the duck with
-      any sort of problem.
-    </p>
-    <br />
-    <p>
-      Begin by explaining your problem to the duck. Remember to be as specific
-      as possible; the duck does not know anything about your problem, you must
-      introduce it to the topic.
-    </p>
-    <br />
-    <p>
-      We often think we understand a problem and fail to realise that we have
-      missed possible solutions. By having to explain the problem to someone who
-      knows nothing about it, one is forced to confront all parts and
-      perspectives of it, which is often enough to find the solution.
-    </p>
-    <br/>
-    <p>
-      See the rubber duck debugging method steps <a href="http://lists.ethernal.org/oldarchives/cantlug-0211/msg00174.html">here</a>. 
-    </p>
-    <h1>About me</h1>
-    <p>Hey, I'm Veronika.</p>
-    <p>
-      I'm a full-stack developer based in Bratislava, Slovakia. Feel free to
-      check out my
-      <a href="https://github.com/vrusi">GitHub</a> or
-      <a href="https://www.linkedin.com/in/veronika-rusinkova-6b012718a/"
-        >LinkedIn</a
-      >. You can also reach me at
-      <a href="mailto: me@vrusinkova.com">me@vrusinkova.com</a>.
-    </p>
-  </div>
+  <main>
+    <div class="container">
+      <h1>About Rubber Duck</h1>
+      <p>
+        This app is here to help you find and solve your problems. Based on the
+        <a href="https://en.wikipedia.org/wiki/Rubber_duck_debugging"
+          >rubber duck debugging method</a
+        >, it is mostly focused on helping software engineers to get rid of the
+        bugs in their code. However, everyone is welcome to consult the duck
+        with any sort of problem.
+      </p>
+      <br />
+      <p>
+        Begin by explaining your problem to the duck. Remember to be as specific
+        as possible; the duck does not know anything about your problem, you
+        must introduce it to the topic.
+      </p>
+      <br />
+      <p>
+        We often think we understand a problem and fail to realise that we have
+        missed possible solutions. By having to explain the problem to someone
+        who knows nothing about it, one is forced to confront all parts and
+        perspectives of it, which is often enough to find the solution.
+      </p>
+      <br />
+      <p>
+        See the rubber duck debugging method steps
+        <a
+          href="http://lists.ethernal.org/oldarchives/cantlug-0211/msg00174.html"
+          >here</a
+        >.
+      </p>
+      <h1>About me</h1>
+      <p>Hey, I'm Veronika.</p>
+      <p>
+        I'm a full-stack developer based in Bratislava, Slovakia. Feel free to
+        check out my
+        <a href="https://github.com/vrusi">GitHub</a> or
+        <a href="https://www.linkedin.com/in/veronika-rusinkova-6b012718a/"
+          >LinkedIn</a
+        >. You can also reach me at
+        <a href="mailto: me@vrusinkova.com">me@vrusinkova.com</a>.
+      </p>
+    </div>
+  </main>
 </template>
 
 <style></style>
